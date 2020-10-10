4b0ac383cfc5dabb62443d30c847b3b2ee1f7015
Sridhar Ratnakumar <srid@srid.ca>
2020-10-09 22:55:41 -0400

    Use closure compiler to optimize JS size
    Almost from 4M to 1M

--- site.nix
+++ site.nix
@@ -7,11 +7,21 @@ let
     inherit system;
   };
   project = import ./default.nix { inherit system; };
-  app = rp.nixpkgs.lib.getAttr name project.ghcjs;
+  pkgs = rp.nixpkgs;
+  app = pkgs.lib.getAttr name project.ghcjs;
   wwwDir = ./www;
 in 
-  rp.nixpkgs.runCommand "${name}-site" {} ''
+  pkgs.runCommand "${name}-site" {} ''
     mkdir -p $out
     cp ${wwwDir}/index.html $out/
-    cp ${app}/bin/${name}.jsexe/all.js $out/
+    # The original all.js is pretty huge; so let's run it by the closure
+    # compiler.    
+    # cp ${app}/bin/${name}.jsexe/all.js $out/
+    ${pkgs.closurecompiler}/bin/closure-compiler \
+        --externs=${app}/bin/${name}.jsexe/all.js.externs \
+        --jscomp_off=checkVars \
+        --js_output_file="$out/all.js" \
+        -O ADVANCED \
+        -W QUIET \
+        ${app}/bin/${name}.jsexe/all.js
   ''
